419183dbd7b693834ca9fcd3548d7f4d88a95cb9
DrCorchit <drcorchit@gmail.com>
2026-05-11 01:31:02 -0400

    Adding placards

--- core/src/main/kotlin/com/drcorchit/cards/cyberpunk/Placard.kt
+++ core/src/main/kotlin/com/drcorchit/cards/cyberpunk/Placard.kt
@@ -16,14 +16,14 @@ class Placard(val bot: Rococobot): Drawable {
     override val outputLocation = "output/images/full/cyberpunk/placard/${name.normalize()}.png"
 
     override fun draw() {
-        val mx = Gdx.input.x.toFloat()
-        val my = Gdx.graphics.height - Gdx.input.y.toFloat()
+        //val mx = Gdx.input.x.toFloat()
+        //val my = Gdx.graphics.height - Gdx.input.y.toFloat()
 
         bot.image.draw(Draw.batch, 270f, 130f)
-        Draw.batch.draw(bot.signature, 300f, 300f)
+        bot.signature.draw(Draw.batch, 700f, 300f, rotation = 20f)
         placard.draw(Draw.batch, 0f, 0f)
 
-        //Draw.drawText(100f, 100f, Fonts.numberFont, "$mx, $my")
+        //Draw.drawText(100f, 100f, Fonts.abilityFont, "$mx, $my")
     }
 
     override fun updateGraphic(): AnimatedSprite {
--- core/src/main/kotlin/com/drcorchit/cards/cyberpunk/Rococobot.kt
+++ core/src/main/kotlin/com/drcorchit/cards/cyberpunk/Rococobot.kt
@@ -2,6 +2,7 @@ package com.drcorchit.cards.cyberpunk
 
 import com.badlogic.gdx.graphics.Color
 import com.drcorchit.cards.LocalAssets
+import com.drcorchit.cards.graphics.AnimatedSprite
 import com.drcorchit.cards.graphics.Textures
 import com.drcorchit.cards.graphics.Textures.asSprite
 
@@ -16,7 +17,7 @@ enum class Rococobot(val color: Color) {
     Sapphire(Color.BLUE);
 
     val image = LocalAssets.getInstance().create("$name.png").asSprite()
-    val image80s = LocalAssets.getInstance().create("${name}_80s.png")
-    val signature = Textures.signatures.getFrames().get(ordinal.toDouble())
+    //val image80s = LocalAssets.getInstance().create("${name}_80s.png").asSprite()
+    val signature = AnimatedSprite(Textures.signatures.getFrames()).let { it.index = ordinal.toDouble(); it }
 
 }
